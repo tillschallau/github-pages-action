a670227fd5d2a134feb74f3659e737bbf84ae860
Till Schallau <till.schallau@tu-dortmund.de>
2021-06-18 11:26:07 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "tools.aqua"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
