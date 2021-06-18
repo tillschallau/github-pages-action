38aba3faa8b73c7dc5ee0c47169c0532c8a06a28
Till Schallau <till.schallau@tu-dortmund.de>
2021-06-18 13:38:56 +0200

    Add simple Documentation.kt class with KotlinDoc

---
+++ src/main/kotlin/Documentation.kt
@@ -0,0 +1,20 @@
+/***
+ * This is the Documentation class
+ *
+ * It is used to check the generation of KotlinDocs
+ *
+ * @param name The name of the documentation
+ * @param length The length of the documentation
+ */
+data class Documentation(var name: String, var length: Int) {
+
+    /**
+     * Adds the given element to the name
+     *
+     * @param element The string that gets attached to the name
+     */
+    fun addElement(element: String){
+        name += element
+    }
+
+}
