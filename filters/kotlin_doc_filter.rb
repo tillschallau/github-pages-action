8a070aa8bcb32808dfd753f51ee05e592c1e1668
tillschallau <9063061+tillschallau@users.noreply.github.com>
2021-06-18 16:29:12 +0000

    Deploying to gh-pages from @ tillschallau/github-pages-action@bc372243db7cbc3fe2437182fd0d08c3bbc0c3ec 🚀

--- filters/kotlin_doc_filter.rb
+++ filters/kotlin_doc_filter.rb
@@ -1,15 +1,16 @@
 module KotlinDocFilter
-    def html(fqn)
-		html = ""
-		fqn.each_char{ |c|
-			if(/[[:upper:]]/.match(c)){
-				html += "-" + c.downcase
-			}else{
-				html += c
-			}
-		}
-		return html
-    end
+  def html(fqn)
+    html = ""
+    fqn.each_char { |c|
+      if (/[[:upper:]]/.match(c))
+        html.concat("-")
+        html.concat(c.downcase)
+      else
+        html.concat(c)
+      end
+    }
+    return html
+  end
 end
 
-Liquid::Template.register_filter(KotlinDocFilter)
+Liquid::Template.register_filter(KotlinDocFilter);
--- index.md
+++ index.md
@@ -2,3 +2,5 @@ Index
 [Kotlin-Docs](./kotlin-docs/)
 
 {{ Documentation | html }}
+{{ uper | upcase }}
+{{ "uper" | upcase }}
